7f23db0ebffacc84e0cd2ff2f1b4b291ce8fbcb0
Suman Saurabh Das <sumansaurabh.das@ad.infosys.com>
2024-12-14 16:11:46 +0530

    Systems design - networking - Creating a TODO app

--- systems-design/networking/rest-api/index.js
+++ systems-design/networking/rest-api/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 /*
   PRE-REQUISITES FOR RUNNING THE EXPRESS APP
@@ -15,7 +16,7 @@ const PORT = 5111;
 // Start server by listening to a port
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
-})
+});
 
 // Configure a route so that, if a client is requesting on that path we will send data.
 
@@ -29,6 +30,40 @@ app.all("/", (req, res) => {
 
   // Sending the information.
   res.send("Server is up and running !");
-})
+});
 
 // To run the app, in terminal run: npm run start.
+
+// -------------------- CREATING A TO-DO API --------------------
+
+// NOTE - Serialized data is exchanged between the resources (client/server). In order to serialize our newTodo data received at server end from client, we need to parse it using body-parser.
+// For this we will use a  package - body-parser, run : npm i body-parser --save
+
+// When using express, we can parse the data received in the body at a central place for each and every request. Here we are using bodyParser here as a middleware i.e. any req that goes to any route will first be parsed by executing below function.
+app.use(bodyParser.json());
+
+// Data
+const todos = [
+  { id: "1", todo: "task 1", completed: false },
+  { id: "2", todo: "task 2", completed: true },
+  { id: "3", todo: "task 3", completed: false },
+]
+
+// READ | GET method - Retrieve data from the server.
+app.get("/todos", (req, res) => {
+  res.json(todos);
+})
+
+// CREATE | POST method - Sends data to the server.
+app.post("/todos", (req, res) => {
+  // In POST request, data has to be sent in the body.
+  const newTodo = req.body;
+  todos.push(newTodo);
+  res.json("New to-do added !");
+})
+
+// In order to make a POST request, we will use postman vs-code extension.
+
+// UPDATE
+
+// DELETE
