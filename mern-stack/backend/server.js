1b97b6f5dbc056c9983836bae601f9691a24973f
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-04-02 00:22:22 +0530

    Express app setup & Added node modules

--- mern-stack/backend/server.js
+++ mern-stack/backend/server.js
@@ -10,31 +10,40 @@
   Open terminal and run: npm install express
   
   Step 3 - Create an express application.
-  Step 4 - Install nodemon globally (npm install -g nodemon) and add a dev script.
-  Step 5 - Run the application using: npm run dev
+  Step 4 - Install nodemon globally: npm install -g nodemon
+  Step 5 - Add a dev script and run the application using: npm run dev
   Step 6 - Setup a route handler to respond to request.
 */
 
- const express = require('express');
+require("dotenv").config(); // Attached environment variables to process.env object.
+const express = require("express");
 
 // Creates an express app & stores the reference in app constant.
 const app = express();
 
-// Middleware: Any code that executes always between getting a request from the server and sending a response to the server.
-// Adding/Registering a global middleware for logging.
-app.use((req, res, next) => {
-  console.log("Request path :",req.path);
-  console.log("Request method :",req.method);
-  // next is a function, that we need to call at the end of the middleware to move on to next piece of middleware i.e. to execute the call back function inside get, post, etc.
-  next();
-})
-
-// Route handler to respond to request.
-app.get("/", (req, res) => {
-  res.json({msg: "Welcome to the app !"})
-})
+/*
+  In order to use a .env file, create the variables containing sensitive data inside .env file.
+  In order to access these environment variables, we need to use a package: npm install dotenv.
+  dotenv is a package that loads environment variables from .env file into process.env object.
+*/
 
 // Listen for requests.
 app.listen(process.env.PORT, () => {
   console.log("Listening on port 4000");
-})
+});
+
+// Route handler to respond to a GET request.
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to the app !" });  // Sends a json string as response.
+});
+
+// After running the server, check response at: http://localhost:4000/
+
+// Middleware: Any code that executes always between getting a request from the server and sending a response to the server.
+// Adding/Registering a global middleware for logging.
+app.use((req, res, next) => {
+  console.log("Request path :", req.path);
+  console.log("Request method :", req.method);
+  // next is a function, that we need to call at the end of the middleware to move on to next piece of middleware i.e. to execute the call back function inside get, post, etc.
+  next();
+});
