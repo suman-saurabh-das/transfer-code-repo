68c9b0714600870d5d4f6ae7247d1ca333777916
Suman Saurabh Das <dsumansaurabh@gmail.com>
2025-04-02 02:24:55 +0530

    MongoDB Atlas & Mongoose

--- mern-stack/backend/server.js
+++ mern-stack/backend/server.js
@@ -15,10 +15,13 @@
   Step 6 - Setup a route handler to respond to request.
 */
 
-require("dotenv").config(); // Attached environment variables to process.env object.
+require("dotenv").config(); // Attaching environment variables to process.env object.
 
 const express = require("express");
 const workoutRoutes = require("./routes/workouts");
+const mongoose = require("mongoose");
+
+// -------------------- CREATING AN EXPRESS APP --------------------
 
 // Creates an express app & stores the reference in app constant.
 const app = express();
@@ -30,9 +33,9 @@ const app = express();
 */
 
 // Listen for requests.
-app.listen(process.env.PORT, () => {
-  console.log("Listening on port 4000");
-});
+// app.listen(process.env.PORT, () => {
+//   console.log("Listening on port 4000");
+// });
 
 // Route handler to respond to a GET request.
 app.get("/", (req, res) => {
@@ -40,6 +43,8 @@ app.get("/", (req, res) => {
 });
 
 // After running the server, check response at: http://localhost:4000/
+
+// -------------------- ADDING MIDDLEWARES --------------------
 
 // Middleware: Any code that executes always between getting a request from the server and sending a response to the server.
 // Adding/Registering a global middleware for logging.
@@ -49,6 +54,8 @@ app.use((req, res, next) => {
   // next is a function, that we need to call at the end of the middleware to move on to next piece of middleware i.e. to execute the call back function inside get, post, etc.
   next();
 });
+
+// -------------------- ADDING ROUTE HANDLERS --------------------
 
 // Using the workoutRoutes in our app.
 // This will grab all the routes defined in workouts.js file and add them to the app.
@@ -60,3 +67,26 @@ app.use("/api/workouts", workoutRoutes);
 // When sending data to the server (POST/PATCH request), we need to access the data from the req object and we can only access that in an express app when we use a middleware called express.json.
 // For any request that comes in, this middleware looks for a body (data), and if it exists, it passes and attaches it to the request object, so that we can access it in the request handler.
 app.use(express.json());
+
+// -------------------- ADDING MONGODB --------------------
+
+/*
+  Create a new database (cluster) in mongoDB.
+  Get the mongoDB connection uri and add it in .env file.
+  Install mongoose package: npm install mongoose
+
+  Mongoose is an ODM (object data model) library, which wraps mongoDB with an extra layer that allows it to use methods to read and write database documents and it also gives us a way to declare models and schemas to ensure a much strict data structure.
+*/
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // Listen to requests only after successful connection to DB.
+    app.listen(process.env.PORT, () => {
+      console.log("Listening on port 4000");
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error :", error);
+  });
